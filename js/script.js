3daa5c8acd87735da267405eb6fe317c04206bdf
Precious Adejumola <olubunmiprecious195@gmail.com>
2024-07-04 12:38:23 +0100

    add code for admissionLetter

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#formOne").submit(function (event) {
+        event.preventDefault();
+        let firstName = $("#firstName").val();
+        let secondName = $("#secondName").val();
+        let institutionName = $("#institutionName").val();
+        let degree = $("#degree").val();
+        let programName = $("#programName").val();
+        let startDate = $("#startDate").val();
+        let gender = $("#gender").val();
+        let country = $("#country").val();
+        $(".container1").toggle();
+        $("input").val("");
+
+
+
+        $(".firstName").text(firstName);
+        $(".secondName").text(secondName);
+        $(".institutionName").text(institutionName);
+        $(".degree").text(degree);
+        $(".programName").text(programName);
+        $(".startDate").text(startDate);
+        $(".gender").text(gender);
+        $(".country").text(country);
+
+        $("#container2").show();
+    })
+    $(".btn-success").click(function(){
+        $("#container2").hide();
+        $(".container1").show();
+    })
+})
